65b7321ccbb80e10fd5d17fdd2b44d62ccd08766
GeorgII <>
2022-09-14 21:40:07 +0300

    changed default dmenu_run options

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-p", "Run:", "-m", dmenumon,  "-l", "10", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include <X11/XF86keysym.h>
